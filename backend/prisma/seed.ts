5c14f1ef2f05ea9ecdbbfd57ddea809da5a8f4d5
emplv <emils@plavenieks.lv>
2023-11-11 15:07:00 +0200

    mocked data

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -7,12 +7,15 @@ async function main() {
     "2bf75b2f-b3cc-4432-bcb9-e4d4174e3b52", // daily
     "7bbdeb1f-b539-489e-9b05-06cf2946969b", // daily
     "3a3a354c-7cfe-4779-92d7-f7e9ca1876cc", // daily
-    "0f6a6377-8609-4a61-a7e6-41a733bd27bb", // daily
-    "0f6a6377-8609-4a61-a7e6-41a733bd27b1", // common
-    "85b2d019-654c-44da-9dba-e8bd683efc33", // first post
-    "441bd88b-86d4-4314-bf28-ee8e3fac739c", // common
-    "d24c4287-1e9e-4246-9938-f76d40bc3e2d", // common
-    "db44f48d-f6b5-40bd-b5ca-dc7c8f4dadb0", // common
+    "0f6a6377-8609-4a61-a7e6-41a733bd27bb", // daily // [3]
+    "0f6a6377-8609-4a61-a7e6-41a733bd27b1", // post // [4]
+    "85b2d019-654c-44da-9dba-e8bd683efc31", // post 5
+    "85b2d019-654c-44da-9dba-e8bd683efc32", // post 6
+    "85b2d019-654c-44da-9dba-e8bd683efc33", // post7
+    "441bd88b-86d4-4314-bf28-ee8e3fac739c", // post8
+    "d24c4287-1e9e-4246-9938-f76d40bc3e2d", // common [9]
+    "db44f48d-f6b5-40bd-b5ca-dc7c8f4dadb0", // common [10]
+    "db44f48d-f6b5-40bd-b5ca-dc7c8f4dadb1", // common [11]
   ]) {
     const video = await prisma.video.upsert({
       where: { uuid: videoId },
@@ -42,13 +45,13 @@ async function main() {
     {
       title: "Third",
       timestamp: 1699783200,
-      date: "2023/11/10",
+      date: "2023/11/12",
       videoId: videos[2].uuid,
     },
     {
-      title: "Fourht",
+      title: "Fourth",
       timestamp: 1699891200,
-      date: "2023/11/10",
+      date: "2023/11/13",
       videoId: videos[3].uuid,
     },
   ]) {
@@ -62,46 +65,42 @@ async function main() {
     });
     challenges.push(item);
   }
-  await prisma.challenge.upsert({
-    where: { videoId: videos[4].uuid },
+
+  let tempChall;
+  tempChall = await prisma.challenge.upsert({
+    where: { videoId: videos[9].uuid },
     update: {},
     create: {
-      title: "Fourht",
-      timestamp: 1699635600,
-      date: "2023/11/10",
-      videoId: videos[4].uuid,
+      title: "Starter",
+      timestamp: 1699705050,
+      date: "2023/11/11",
+      videoId: videos[9].uuid,
     },
   });
-  await prisma.challenge.upsert({
-    where: { videoId: videos[6].uuid },
+  challenges.push(tempChall);
+  tempChall = prisma.challenge.upsert({
+    where: { videoId: videos[10].uuid },
     update: {},
     create: {
       title: "Wiggly",
-      timestamp: 1699705050,
+      timestamp: 1699708050,
       date: "2023/11/11",
-      videoId: videos[6].uuid,
+      videoId: videos[10].uuid,
     },
   });
-  await prisma.challenge.upsert({
-    where: { videoId: videos[7].uuid },
+  challenges.push(tempChall);
+  tempChall = prisma.challenge.upsert({
+    where: { videoId: videos[11].uuid },
     update: {},
     create: {
       title: "Energetic vibe",
-      timestamp: 1699705080,
-      date: "2023/11/11",
-      videoId: videos[7].uuid,
+      timestamp: 1699908050,
+      date: "2023/11/12",
+      videoId: videos[11].uuid,
     },
   });
-  await prisma.challenge.upsert({
-    where: { videoId: videos[8].uuid },
-    update: {},
-    create: {
-      title: "Balance",
-      timestamp: 1699705090,
-      date: "2023/11/11",
-      videoId: videos[8].uuid,
-    },
-  });
+  challenges.push(tempChall);
+
   // Users
   const user = await prisma.user.upsert({
     where: { id: 1 },
@@ -110,6 +109,33 @@ async function main() {
       id: 1,
       email: "stick@team",
       name: "StickTeam",
+    },
+  });
+  const user2 = await prisma.user.upsert({
+    where: { id: 2 },
+    update: {},
+    create: {
+      id: 2,
+      email: "stick2@team",
+      name: "Jemma",
+    },
+  });
+  const user3 = await prisma.user.upsert({
+    where: { id: 3 },
+    update: {},
+    create: {
+      id: 3,
+      email: "stick3@team",
+      name: "Stefanny",
+    },
+  });
+  const user4 = await prisma.user.upsert({
+    where: { id: 4 },
+    update: {},
+    create: {
+      id: 4,
+      email: "stick24@team",
+      name: "Katty",
     },
   });
   // Posts
@@ -123,8 +149,64 @@ async function main() {
       score: 99,
       authorId: user.id,
       challengeId: challenges[0].id,
+      videoId: videos[4].uuid,
+      reactions: 3,
+    },
+  });
+  await prisma.post.upsert({
+    where: { id: 2 },
+    update: {},
+    create: {
+      id: 2,
+      timestamp: 1699653765,
+      title: challenges[1].title,
+      score: 78,
+      authorId: user.id,
+      challengeId: challenges[1].id,
       videoId: videos[5].uuid,
       reactions: 3,
+    },
+  });
+  await prisma.post.upsert({
+    where: { id: 3 },
+    update: {},
+    create: {
+      id: 3,
+      timestamp: 1699656465,
+      title: challenges[1].title,
+      score: 96,
+      authorId: user2.id,
+      challengeId: challenges[1].id,
+      videoId: videos[6].uuid,
+      reactions: 78,
+    },
+  });
+  await prisma.post.upsert({
+    where: { id: 4 },
+    update: {},
+    create: {
+      id: 4,
+      timestamp: 1699653495,
+      title: challenges[1].title,
+      score: 32,
+      authorId: user3.id,
+      challengeId: challenges[1].id,
+      videoId: videos[7].uuid,
+      reactions: 3,
+    },
+  });
+  await prisma.post.upsert({
+    where: { id: 5 },
+    update: {},
+    create: {
+      id: 5,
+      timestamp: 1699673025,
+      title: challenges[4].title,
+      score: 88,
+      authorId: user3.id,
+      challengeId: challenges[4].id,
+      videoId: videos[8].uuid,
+      reactions: 5,
     },
   });
 }

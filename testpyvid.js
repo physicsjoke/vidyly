cdd707769bab42e62a8c5c2942835371fd426fc9
emplv <emils@plavenieks.lv>
2023-11-11 17:13:53 +0200

    update test script to install all deps for python

--- testpyvid.js
+++ testpyvid.js
@@ -3,9 +3,18 @@ const path = require("path");
 let start = Date.now();
 const pythonProcess = spawnSync("python3", [
   path.normalize(path.join(__dirname, "./backend/services/compare.py")),
-  path.normalize(path.join(__dirname, "./backend/media/d24c4287-1e9e-4246-9938-f76d40bc3e2d.mp4")),
-  path.normalize(path.join(__dirname, "./backend/media/copy.mp4")),
-  path.normalize(path.join(__dirname, "./backend/media/pose.mp4")),
+  path.normalize(
+    path.join(
+      __dirname,
+      "./backend/media/d24c4287-1e9e-4246-9938-f76d40bc3e2d.mp4"
+    )
+  ),
+  path.normalize(
+    path.join(
+      __dirname,
+      "./backend/media/db44f48d-f6b5-40bd-b5ca-dc7c8f4dadb1.mp4"
+    )
+  ),
 ]);
 const result = pythonProcess.stdout?.toString()?.trim();
 const error = pythonProcess.stderr?.toString()?.trim();
@@ -17,4 +26,4 @@ if (result && (score === 0 || score)) {
 } else {
   console.log(error);
 }
-console.log((Date.now() - start) / 1000 | 0);
+console.log("time spent: ", ((Date.now() - start) / 1000) | 0);

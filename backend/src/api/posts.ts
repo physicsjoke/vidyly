67e078d02c96e292e0c04b273904f4bd1941539e
emplv <emils@plavenieks.lv>
2023-11-11 15:16:46 +0200

    1

--- backend/src/api/posts.ts
+++ backend/src/api/posts.ts
@@ -29,67 +29,75 @@ postsRouter.get("/", async (req, res) => {
   }
 });
 
-postsRouter.post("/", videoUpload.single("video"), async (req, res) => {
-  try {
-    const { challengeId, authorId, videoUuid } = req.body;
-    console.log({ challengeId, authorId, videoUuid, fp: req.file?.path });
-    if (req.file?.path == null) {
-      res.status(400);
-      res.json({
-        status: "Upload failed",
+postsRouter.post(
+  "/",
+  (req, res, next) => {
+    console.log({ reqbody: req.body, req });
+    next();
+  },
+  videoUpload.single("video"),
+  async (req, res) => {
+    try {
+      const { challengeId, authorId, videoUuid } = req.body;
+      console.log({ challengeId, authorId, videoUuid, fp: req.file?.path });
+      if (req.file?.path == null) {
+        res.status(400);
+        res.json({
+          status: "Upload failed",
+        });
+        return;
+      }
+      const challenge = await DB.prisma.challenge.findUnique({
+        where: {
+          id: Number(challengeId),
+        },
       });
+      if (challenge == null) {
+        res.status(400);
+        res.json({
+          status: "Challenge not found",
+        });
+        return;
+      }
+      const existingPost = await DB.prisma.post.findFirst({
+        where: {
+          authorId: Number(authorId),
+          challengeId: Number(challengeId),
+        },
+      });
+      if (existingPost != null) {
+        res.status(400);
+        res.json({
+          status: "Challenge has been already completed before",
+        });
+        return;
+      }
+      // TODO service through video to stick & save
+      let score = Math.min(Math.floor(Math.random() * 100 + 1), 100);
+      await DB.prisma.video.create({
+        data: {
+          uuid: videoUuid,
+          path: mediaPath(videoUuid),
+        },
+      });
+      const post = await DB.prisma.post.create({
+        data: {
+          timestamp: (Date.now() / 1000) | 0,
+          title: challenge.title,
+          score,
+          authorId: Number(authorId),
+          challengeId: Number(challengeId),
+          videoId: videoUuid,
+        },
+      });
+      res.json(post);
+    } catch (err) {
+      console.log(err);
+      res.status(500);
+      res.end();
       return;
     }
-    const challenge = await DB.prisma.challenge.findUnique({
-      where: {
-        id: Number(challengeId),
-      },
-    });
-    if (challenge == null) {
-      res.status(400);
-      res.json({
-        status: "Challenge not found",
-      });
-      return;
-    }
-    const existingPost = await DB.prisma.post.findFirst({
-      where: {
-        authorId: Number(authorId),
-        challengeId: Number(challengeId),
-      },
-    });
-    if (existingPost != null) {
-      res.status(400);
-      res.json({
-        status: "Challenge has been already completed before",
-      });
-      return;
-    }
-    // TODO service through video to stick & save
-    let score = Math.min(Math.floor(Math.random() * 100 + 1), 100);
-    await DB.prisma.video.create({
-      data: {
-        uuid: videoUuid,
-        path: mediaPath(videoUuid),
-      },
-    });
-    const post = await DB.prisma.post.create({
-      data: {
-        timestamp: (Date.now() / 1000) | 0,
-        title: challenge.title,
-        score,
-        authorId: Number(authorId),
-        challengeId: Number(challengeId),
-        videoId: videoUuid,
-      },
-    });
-    res.json(post);
-  } catch (err) {
-    console.log(err);
-    res.status(500);
-    res.end();
-    return;
   }
-});
+);
 
 export default postsRouter;
